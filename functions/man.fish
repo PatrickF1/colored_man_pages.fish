0ab64545c3e4ad029c779a57f5229f207464ab61
Kid <44045911+kidonng@users.noreply.github.com>
2021-05-06 17:16:53 +0800

    Add Fish's man directory to `$MANPATH`

--- functions/man.fish
+++ functions/man.fish
@@ -1,3 +1,14 @@
 function man --wraps man -d "Run man with added colors"
-    cless /usr/bin/man $argv
+    set --local --export MANPATH $MANPATH
+
+    if test -z "$MANPATH" && set path (command man -p 2>/dev/null)
+        set MANPATH (string replace --regex '[^/]+$' '' $path)
+    end
+
+    set fish_manpath (dirname $__fish_data_dir)/fish/man
+    if test -d $fish_manpath
+        set --prepend MANPATH $fish_manpath
+    end
+
+    cless (command --search man) $argv
 end
