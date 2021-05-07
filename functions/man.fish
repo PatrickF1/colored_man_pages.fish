f6b4774db541d9e9b36e87278858d597d5626da7
Patrick Fong <patrickf3139@gmail.com>
2021-05-06 21:19:35 -0700

    comments

--- functions/man.fish
+++ functions/man.fish
@@ -1,10 +1,12 @@
 function man --wraps man -d "Run man with added colors"
     set --local --export MANPATH $MANPATH
 
+    # set MANPATH if not already set to ??? TODO
     if test -z "$MANPATH" && set path (command man -p 2>/dev/null)
         set MANPATH (string replace --regex '[^/]+$' '' $path)
     end
 
+    # prepend the directory of fish manpages to MANPATH
     set fish_manpath (dirname $__fish_data_dir)/fish/man
     if test -d $fish_manpath
         set --prepend MANPATH $fish_manpath
