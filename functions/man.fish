038bb06cd5084efe0243b2a2c4491b56960864e7
Kid <44045911+kidonng@users.noreply.github.com>
2021-05-20 23:04:23 +0800

    Oops

--- functions/man.fish
+++ functions/man.fish
@@ -1,10 +1,14 @@
 function man --wraps man -d "Run man with added colors"
-    set --local --export MANPATH $MANPATH ''
+    set --local --export MANPATH $MANPATH
 
     # special case for NetBSD and FreeBSD: set MANPATH if not already set
     # see https://github.com/fish-shell/fish-shell/blob/555af37616893160ad1afb208a957d6a01a7a315/share/functions/man.fish#L15
-    if test -z "$MANPATH" && set path (command man -p 2>/dev/null)
-        set MANPATH (string replace --regex '[^/]+$' '' $path)
+    if test -z "$MANPATH"
+        if set path (command man -p 2>/dev/null)
+            set MANPATH (string replace --regex '[^/]+$' '' $path)
+        else
+            set MANPATH ""
+        end
     end
 
     # prepend the directory of fish manpages to MANPATH
