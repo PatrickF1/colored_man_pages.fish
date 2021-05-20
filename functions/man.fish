c0be9282edd43d2b586f513744b6466c90f13191
Kid <44045911+kidonng@users.noreply.github.com>
2021-05-21 00:13:37 +0800

    Fix manpages for non-fish commands (#5)
    Fixes #4 . Handle empty `MANPATH` like [Fish](https://github.com/fish-shell/fish-shell/blob/504a969a2471b7e2ed459d7c2b3b026919ce5f3a/share/functions/man.fish) does.
    
    I also took the chance to backport [another change in fish](https://github.com/fish-shell/fish-shell/commit/2cfb4343ed49aa976c8f6e5fc999198c0d873ef7#diff-e2c1373c8715bec537d5247400fe5f8de23eceaa2a1d79b61e90aaaf919c5aaa).

--- functions/man.fish
+++ functions/man.fish
@@ -3,12 +3,16 @@ function man --wraps man -d "Run man with added colors"
 
     # special case for NetBSD and FreeBSD: set MANPATH if not already set
     # see https://github.com/fish-shell/fish-shell/blob/555af37616893160ad1afb208a957d6a01a7a315/share/functions/man.fish#L15
-    if test -z "$MANPATH" && set path (command man -p 2>/dev/null)
-        set MANPATH (string replace --regex '[^/]+$' '' $path)
+    if test -z "$MANPATH"
+        if set path (command man -p 2>/dev/null)
+            set MANPATH (string replace --regex '[^/]+$' '' $path)
+        else
+            set MANPATH ""
+        end
     end
 
     # prepend the directory of fish manpages to MANPATH
-    set fish_manpath (dirname $__fish_data_dir)/fish/man
+    set fish_manpath $__fish_data_dir/man
     if test -d $fish_manpath
         set --prepend MANPATH $fish_manpath
     end
